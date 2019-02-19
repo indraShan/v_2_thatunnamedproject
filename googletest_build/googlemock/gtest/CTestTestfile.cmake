42b9bb55b9b61b234cc73c646bbb5caa0feaebe3
indrajit <indrashanbhag@gmail.com>
2019-02-18 21:30:05 -0500

    Removing g test integration

--- googletest_build/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/indrajit/Documents/Study/UFL/Spring19/DBI/Project/2/P2/googletest/googletest
-# Build directory: /Users/indrajit/Documents/Study/UFL/Spring19/DBI/Project/2/P2/googletest_build/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
